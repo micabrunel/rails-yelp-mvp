f6153620c9dc59a55b6421068b191f3b59bebf2f
Michael Brunel <brunel.michael@gmail.com>
2019-08-07 14:52:38 +0200

    second exo done without views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Chez le jap",
+    address:      '7 rue de Tokyo, Marseille',
+    phone_number: '0491678986',
+    category:     'japanese'
+  },
+  {
+    name:         "Pizza Pino",
+    address:      'Little Italy, Marseille',
+    phone_number: '049164566',
+    category:     'italian'
+  },
+   {
+    name:         "L'entrecote",
+    address:      '23 rue paradis, Marseille',
+    phone_number: '049134566',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+
+puts 'Creating reviews...'
+@restaurant = Restaurant.all
+reviews_attributes = [
+  {
+    content:      "C'etait super bon !",
+    rating:       5,
+    restaurant: @restaurant.sample
+  },
+  {
+    content:      "Bof",
+    rating:       2,
+    restaurant: @restaurant.sample
+  },
+  {
+    content:      "Moyen",
+    rating:       3,
+    restaurant: @restaurant.sample
+  },
+  {
+    content:      "Amazing",
+    rating:       5,
+    restaurant: @restaurant.sample
+  },
+  {
+    content:      "Great !",
+    rating:       4,
+    restaurant: @restaurant.sample
+  },
+  {
+    content:      "Ok",
+    rating:       3,
+    restaurant: @restaurant.sample
+  },
+  {
+    content:      "C'etait super!",
+    rating:       5,
+    restaurant: @restaurant.sample
+  },
+]
+Review.create!(reviews_attributes)
+puts 'Finished!'
